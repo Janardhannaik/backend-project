e428c0544d11fad28fee90f105b956a5e5851cd1
JANARDHAN NAIK <naikjanardhan568@gmail.com>
2024-04-14 12:31:29 +0530

    connect mongoatlas

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+(async()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log("err",error)
+        throw error
+       })
+
+       app.listen(process.env.PORT, ()=>{
+        console.log(`App is listening on port ${process.env.PORT}`);
+       })
+        
+    } catch (error) {
+        console.error("error:",error)
+        throw err
+    }
+})()*/
