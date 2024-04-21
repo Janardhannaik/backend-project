4010c4911e1f65150a6a2d88ad30140a67e53fd9
JANARDHAN NAIK <naikjanardhan568@gmail.com>
2024-04-21 18:03:12 +0530

    utis code discussion

--- src/index.js
+++ src/index.js
@@ -1,41 +1,19 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`SERVER is running at port:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB CONNECT FAILED", error);
+  });
 
 /*
 import express from "express";
